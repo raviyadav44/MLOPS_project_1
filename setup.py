9b60072916d8f61fa31ce041d5a5766027d957e8
raviyadav44 <raviyadav68358@gmail.com>
2025-06-09 09:17:20 +0530

    project structure ready,vevn set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ravi Yadav",
+    author_email="raviyadav6858@gmail.com",
+    packages=find_packages()
+)
